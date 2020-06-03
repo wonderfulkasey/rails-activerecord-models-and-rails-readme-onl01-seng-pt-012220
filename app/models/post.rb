fe9673adcde13c4cdbc57aeb97e2fe3dd9427bad
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-03 00:23:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-    def post_summary
+  def post_summary
         self.title + " - " + self.description
       end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe Post do
-    it 'can be created' do
-        post = Post.create!(title: "My title", description: "The post description")
-        expect(post.post_summary).to eq("My title - The post description")
+  it 'can be created' do
+      post = Post.create!(title: "My title", description: "The post description")
+      expect(post.post_summary).to eq("My title - The post description")
     end
 end
