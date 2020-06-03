74f92c3611d2880191ed563cc2476526170b5c9a
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-03 00:25:24 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Post do
-  it 'can be created' do
+  #it 'can be created' do
       post = Post.create!(title: "My title", description: "The post description")
       expect(post.post_summary).to eq("My title - The post description")
     end
